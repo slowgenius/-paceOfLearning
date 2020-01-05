3a04f3b55eb6e329fba804f8af8584ad3a5c774e
slowgenius <305109030@qq.com>
2020-01-05 15:13:44 +0800

    测试git

---
+++ src/main/java/com/slowgenius/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.slowgenius;
+
+/**
+ * @author slowgenius
+ * @date 12/26/2019 3:41 PM
+ * @description
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
